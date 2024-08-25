cb02695cd2793ce99eeb980b014fa88bb0459852
Oleg <69583657+OTube@users.noreply.github.com>
2024-08-25 11:52:12 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -5,17 +5,7 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        self.wfile.write("forbidden".encode('utf-8'))
-        return
         if self.path.count("op")<2:
             self.wfile.write("forbidden".encode('utf-8'))
             return
-        op=self.path.split("op")[1]
-        if op=="setlot":
-            return
-        else if op=="getlot":
-            return
-        else if op=="checkstatus":
-            return
-        else:
-            return
+        
