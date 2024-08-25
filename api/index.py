f30ed9d27182e0b83d5ddc1cbc3148532cc8cdeb
Oleg <69583657+OTube@users.noreply.github.com>
2024-08-25 11:58:38 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -9,4 +9,30 @@ class handler(BaseHTTPRequestHandler):
             self.wfile.write("forbidden".encode('utf-8'))
             return
         op=self.path.split("op")[1]
-        self.wfile.write(op.encode('utf-8'))
+        if op=="setlot":
+            if self.path.count("token")<2:
+                self.wfile.write("forbidden".encode('utf-8'))
+                return
+            token=self.path.split("token")[1]
+            if self.path.count("value")<2:
+                self.wfile.write("forbidden".encode('utf-8'))
+                return
+            value=self.path.split("value")[1]
+            lots[token]=value
+        elif op=="getlot":
+            if self.path.count("token")<2:
+                self.wfile.write("forbidden".encode('utf-8'))
+                return
+            token=self.path.split("token")[1]
+            if token in lots:
+                self.wfile.write(str(lots[token]).encode('utf-8'))
+                del lots[token]
+            else:
+                self.wfile.write("error".encode('utf-8'))
+        elif op=="checkstatus":
+            if token in lots:
+                self.wfile.write("yes".encode('utf-8'))
+            else:
+                self.wfile.write("no".encode('utf-8'))
+        else:
+            self.wfile.write("forbidden".encode('utf-8'))
