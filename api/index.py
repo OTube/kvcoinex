5e99993a657aece7266736d43a610a2d06f09d3e
Oleg <69583657+OTube@users.noreply.github.com>
2024-08-25 11:48:00 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -5,12 +5,24 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
+        if self.path.count("op")<2:
+            self.wfile.write("forbidden".encode('utf-8'))
+            return
         op=self.path.split("op")[1]
         if op=="setlot":
+            if self.path.count("token")<2:
+                self.wfile.write("forbidden".encode('utf-8'))
+                return
             token=self.path.split("token")[1]
+            if self.path.count("value")<2:
+                self.wfile.write("forbidden".encode('utf-8'))
+                return
             value=self.path.split("value")[1]
             lots[token]=value
         else if op=="getlot":
+            if self.path.count("token")<2:
+                self.wfile.write("forbidden".encode('utf-8'))
+                return
             token=self.path.split("token")[1]
             if token in lots:
                 self.wfile.write(str(lots[token]).encode('utf-8'))
